4eeb45a44e2db2b82f1def19f727ccf37116a887
Caleb King <calebking9876@gmail.com>
2026-03-04 11:50:11 +1300

    Update remove_extra_spaces_function.py

--- remove_extra_spaces_function.py
+++ remove_extra_spaces_function.py
@@ -9,10 +9,10 @@ def remove_extra_spaces_function(processed_duplicates_df):
   # SPACES AT START AND END
 
   # count number of cells with extra spaces at the start or end
-  leading_trailing_spaces_unprocessed = unprocessed_spaces_df.apply(lambda col: col.astype(str).str.contains(r'^\s+|\s+$',na=False)).sum().sum()
+  leading_trailing_spaces_unprocessed = unprocessed_spaces_df.apply(lambda col: col.astype(str).str.contains(r'(?m)^[ \t]+|[ \t]+$',na=False)).sum().sum()
 
   # removes extra spaces at the begining and end
-  unprocessed_spaces_df[unprocessed_spaces_df.columns] = unprocessed_spaces_df.apply(lambda x: x.str.strip())
+  processed_spaces_df = unprocessed_spaces_df.replace(r'(?m)^[ \t]+|[ \t]+$', '', regex=True)
 
   # count number of cells with extra spaces at the start or end
   leading_trailing_spaces_processed = unprocessed_spaces_df.apply(lambda col: col.astype(str).str.contains(r'^\s+|\s+$',na=False)).sum().sum()
@@ -25,7 +25,7 @@ def remove_extra_spaces_function(processed_duplicates_df):
   extra_spaces_output_unprocessed = unprocessed_spaces_df['output'].astype(str).apply(lambda x: sum(len(m) - 1 for m in re.findall(r'[ ]{2,}', x))).sum()
 
   # removes the extra spaces except extra lines
-  processed_spaces_df = unprocessed_spaces_df.astype(str).replace(r'[ \t]+', ' ', regex=True)
+  processed_spaces_df = unprocessed_spaces_df.astype(str).replace(r'[ \t]{2,}', ' ', regex=True)
   
   # count total number of extra spaces per column
   extra_spaces_instruction_processed = processed_spaces_df['instruction'].astype(str).apply(lambda x: sum(len(m) - 1 for m in re.findall(r'[ ]{2,}', x))).sum()
