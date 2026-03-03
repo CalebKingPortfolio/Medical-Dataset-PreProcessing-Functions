51e2a36c48eadf1212812e0b88ca3476c5eb8bb7
Caleb King <calebking9876@gmail.com>
2026-03-03 14:15:11 +1300

    Rename variable for clarity on space processing

--- remove_extra_spaces_function.py
+++ remove_extra_spaces_function.py
@@ -15,7 +15,7 @@ def remove_extra_spaces_function(processed_duplicates_df):
   unprocessed_spaces_df[unprocessed_spaces_df.columns] = unprocessed_spaces_df.apply(lambda x: x.str.strip())
 
   # count number of cells with extra spaces at the start or end
-  cells_with_extra_spaces_processed = unprocessed_spaces_df.apply(lambda col: col.astype(str).str.contains(r'^\s+|\s+$',na=False)).sum().sum()
+  leading_trailing_spaces_processed = unprocessed_spaces_df.apply(lambda col: col.astype(str).str.contains(r'^\s+|\s+$',na=False)).sum().sum()
 
   # SPACES IN MIDDLE 
 
@@ -32,4 +32,4 @@ def remove_extra_spaces_function(processed_duplicates_df):
   extra_spaces_ds = len(processed_spaces_df)
   
   # returns variables used within the function
-  return(cells_with_extra_spaces_unprocessed, cells_with_extra_spaces_processed,unprocessed_total_matches,processed_total_matches, extra_spaces_ds, processed_spaces_df)
+  return(leading_trailing_spaces_unprocessed,leading_trailing_spaces_processed,unprocessed_total_matches,processed_total_matches, extra_spaces_ds, processed_spaces_df)
