16746c3b6dfa9845946474cb10d8c35dec92e19b
Caleb King <calebking9876@gmail.com>
2026-03-11 12:28:30 +1300

    Refactor comments in remove_qm_flashcards_function
    Updated comments for clarity and improved code readability.

--- remove_qm_flashcards_function.py
+++ remove_qm_flashcards_function.py
@@ -3,25 +3,25 @@ import re
 
 def remove_qm_flashcards_function(processed_duplicates_df):
 
-  # gives the dataframe another name
-  unprocessed_qm_output_df = processed_duplicates_df
+  # creates another copy of the dataframe
+  unprocessed_qm_output_df = processed_duplicates_df.copy()
 
-  # gets the original size of the dataset
+  # gets the length of the unprocessed dataframe
   unprocessed_fs_ds = len(unprocessed_qm_output_df)
 
-  # gets the subset  used
+  # get the length of rows from the 'medical_meadow_medical_flashcards' subset
   unprocessed_qm_ds = (unprocessed_qm_output_df['subset_source'] == 'medical_meadow_medical_flashcards').sum()
 
-  # the subset getting checked
+  # sets the subset getting fixed
   flashcards_subset = unprocessed_qm_output_df['subset_source'] == 'medical_meadow_medical_flashcards' 
 
-  # strips the output
+  # strips the output of subset medical_meadow_medical_flashcards
   unprocessed_qm_output_df.loc[flashcards_subset, 'output'] = unprocessed_qm_output_df.loc[flashcards_subset, 'output'].astype(str).str.strip()
   
   # gets the length of the subset with question marks (using \s* to catch hidden spaces)
   qm_count_unprocessed = unprocessed_qm_output_df[flashcards_subset]['output'].astype(str).apply(lambda x: len(re.findall(r'\?\s*$', x))).sum()
 
-  # checks for which output lines end in question marks (updated regex to match the count logic)
+  # checks which output lines end in question marks 
   unprocessed_qm = unprocessed_qm_output_df[flashcards_subset]['output'].astype(str).str.contains(r'\?\s*$', regex=True)
 
   # gets the index value of the outputs ending in question marks
@@ -36,7 +36,8 @@ def remove_qm_flashcards_function(processed_duplicates_df):
   # gets the original size of the dataset
   processed_qm_ds = (processed_qm_output_df['subset_source'] == 'medical_meadow_medical_flashcards').sum()
 
-  # gets the original size of the dataset
+  # gets the length of the processed dataframe
   processed_fs_ds = len(processed_qm_output_df)
-
+  
+  # return the variables that are used inside this function
   return(unprocessed_fs_ds, processed_fs_ds, unprocessed_qm_ds, qm_count_unprocessed, qm_count_processed, processed_qm_ds, processed_qm_output_df)
